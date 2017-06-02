495ba74f1667591d46a556a840981d644dda5e99
Messer <simon.messer@adesso.de>
2017-06-02 13:28:08 +0200

    removed tabs use cards instead

--- angular/src/app/season/season.component.ts
+++ angular/src/app/season/season.component.ts
@@ -12,4 +12,134 @@ export class SeasonComponent implements OnInit {
   ngOnInit() {
   }
 
+  public show = {
+    'id': 1,
+    'name': 'Game of Thrones',
+    'image': "http://thetvdb.com/banners/fanart/original/121361-15.jpg",
+    'titleimage': "https://red.elbenwald.de/media/image/3a/49/87/game-of-thrones_cat.jpg",
+    'episodes': 70,
+    'viewers': 51301,
+    'rating': {
+      '1': 5,
+      '2': 57,
+      '3': 72,
+      '4': 322,
+      '5': 263,
+      '6': 886,
+      '7': 1201,
+      '8': 2200,
+      '9': 2301,
+      '10': 1900
+    },
+    'seasons': [{
+      id: 1,
+      episodes: [{
+        id: 1,
+        name: 'Test'
+      }, {
+        id: 2,
+        name: 'Test'
+      }, {
+        id: 3,
+        name: 'Test'
+      }, {
+        id: 4,
+        name: 'Test'
+      }, {
+        id: 5,
+        name: 'Test'
+      }]
+    }, {
+      id: 2,
+      episodes: [{
+        id: 1,
+        name: 'Test'
+      }, {
+        id: 2,
+        name: 'Test'
+      }, {
+        id: 3,
+        name: 'Test'
+      }, {
+        id: 4,
+        name: 'Test'
+      }, {
+        id: 5,
+        name: 'Test'
+      }]
+    },{
+      id: 3,
+      episodes: [{
+        id: 1,
+        name: 'Test'
+      }, {
+        id: 2,
+        name: 'Test'
+      }, {
+        id: 3,
+        name: 'Test'
+      }, {
+        id: 4,
+        name: 'Test'
+      }, {
+        id: 5,
+        name: 'Test'
+      }]
+    },{
+      id: 4,
+      episodes: [{
+        id: 1,
+        name: 'Test'
+      }, {
+        id: 2,
+        name: 'Test'
+      }, {
+        id: 3,
+        name: 'Test'
+      }, {
+        id: 4,
+        name: 'Test'
+      }, {
+        id: 5,
+        name: 'Test'
+      }]
+    },{
+      id: 5,
+      episodes: [{
+        id: 1,
+        name: 'Test'
+      }, {
+        id: 2,
+        name: 'Test'
+      }, {
+        id: 3,
+        name: 'Test'
+      }, {
+        id: 4,
+        name: 'Test'
+      }, {
+        id: 5,
+        name: 'Test'
+      }]
+    },{
+      id: 6,
+      episodes: [{
+        id: 1,
+        name: 'Test'
+      }, {
+        id: 2,
+        name: 'Test'
+      }, {
+        id: 3,
+        name: 'Test'
+      }, {
+        id: 4,
+        name: 'Test'
+      }, {
+        id: 5,
+        name: 'Test'
+      }]
+    }]
+  };
+
 }
