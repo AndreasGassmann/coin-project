e1869b57b3e517d0e887617d2568bf8106eaa39a
Messer <simon.messer@adesso.de>
2017-06-02 10:56:34 +0200

    let frontend use mock data Mock some more api data

--- api.js
+++ api.js
@@ -9,6 +9,7 @@ let shows = [{
     'id': 1,
     'name': 'Game of Thrones',
     'image': "http://thetvdb.com/banners/fanart/original/121361-15.jpg",
+    'titleimage': "https://red.elbenwald.de/media/image/3a/49/87/game-of-thrones_cat.jpg",
     'episodes': 70,
     'viewers': 51301,
     'rating': {
@@ -63,11 +64,169 @@ let shows = [{
 }, {
     'id': 2,
     'name': 'The Big Bang Theory',
-    'image': "http://thetvdb.com/banners/fanart/original/80379-38.jpg"
+    'image': "http://thetvdb.com/banners/fanart/original/80379-38.jpg",
+    'titleimage': "https://red.elbenwald.de/media/image/e5/96/fc/big-bang-theorycat.jpg",
+    'episodes': 70,
+    'viewers': 51301,
+    'rating': {
+        '1': 5,
+        '2': 57,
+        '3': 72,
+        '4': 322,
+        '5': 263,
+        '6': 886,
+        '7': 1201,
+        '8': 2200,
+        '9': 2301,
+        '10': 1900
+    },
+    'seasons': [{
+        id: 1,
+        episodes: [{
+            id: 1,
+            name: 'Test'
+        }, {
+            id: 2,
+            name: 'Test'
+        }, {
+            id: 3,
+            name: 'Test'
+        }, {
+            id: 4,
+            name: 'Test'
+        }, {
+            id: 5,
+            name: 'Test'
+        }]
+    }, {
+        id: 2,
+        episodes: [{
+            id: 1,
+            name: 'Test'
+        }, {
+            id: 2,
+            name: 'Test'
+        }, {
+            id: 3,
+            name: 'Test'
+        }, {
+            id: 4,
+            name: 'Test'
+        }, {
+            id: 5,
+            name: 'Test'
+        }]
+    }],
 }, {
     'id': 3,
     'name': '13 Reasons Why',
-    'image': 'http://thetvdb.com/banners/fanart/original/323168-10.jpg'
+    'image': "http://thetvdb.com/banners/fanart/original/323168-10.jpg",
+    'episodes': 70,
+    'viewers': 51301,
+    'rating': {
+        '1': 5,
+        '2': 57,
+        '3': 72,
+        '4': 322,
+        '5': 263,
+        '6': 886,
+        '7': 1201,
+        '8': 2200,
+        '9': 2301,
+        '10': 1900
+    },
+    'seasons': [{
+        id: 1,
+        episodes: [{
+            id: 1,
+            name: 'Test'
+        }, {
+            id: 2,
+            name: 'Test'
+        }, {
+            id: 3,
+            name: 'Test'
+        }, {
+            id: 4,
+            name: 'Test'
+        }, {
+            id: 5,
+            name: 'Test'
+        }]
+    }, {
+        id: 2,
+        episodes: [{
+            id: 1,
+            name: 'Test'
+        }, {
+            id: 2,
+            name: 'Test'
+        }, {
+            id: 3,
+            name: 'Test'
+        }, {
+            id: 4,
+            name: 'Test'
+        }, {
+            id: 5,
+            name: 'Test'
+        }]
+    }],
+}, {
+    'id': 4,
+    'name': 'Criminal Minds',
+    'image': "http://www.returndates.com/backgrounds/criminalminds.jpg",
+    'episodes': 70,
+    'viewers': 51301,
+    'rating': {
+        '1': 5,
+        '2': 57,
+        '3': 72,
+        '4': 322,
+        '5': 263,
+        '6': 886,
+        '7': 1201,
+        '8': 2200,
+        '9': 2301,
+        '10': 1900
+    },
+    'seasons': [{
+        id: 1,
+        episodes: [{
+            id: 1,
+            name: 'Test'
+        }, {
+            id: 2,
+            name: 'Test'
+        }, {
+            id: 3,
+            name: 'Test'
+        }, {
+            id: 4,
+            name: 'Test'
+        }, {
+            id: 5,
+            name: 'Test'
+        }]
+    }, {
+        id: 2,
+        episodes: [{
+            id: 1,
+            name: 'Test'
+        }, {
+            id: 2,
+            name: 'Test'
+        }, {
+            id: 3,
+            name: 'Test'
+        }, {
+            id: 4,
+            name: 'Test'
+        }, {
+            id: 5,
+            name: 'Test'
+        }]
+    }],
 }];
 
 router.get('/', async(ctx, next) => {
