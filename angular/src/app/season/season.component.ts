334caf60401c1e552b7a24eb29893a509acf2ed3
Messer <simon.messer@adesso.de>
2017-06-02 14:37:12 +0200

    Season level avg and distribution.

--- angular/src/app/season/season.component.ts
+++ angular/src/app/season/season.component.ts
@@ -12,6 +12,70 @@ export class SeasonComponent implements OnInit {
   ngOnInit() {
   }
 
+  // Rating Distribution
+  public ratingDistributionOptions: any = {
+    scaleShowVerticalLines: false,
+    responsive: true,
+  };
+  public ratingDistributionColors: Array<any> = [
+    { // yellow
+      backgroundColor: 'rgba(248, 203, 0, 0.75)',
+      borderColor: 'rgba(211,165,0,1)',
+      pointBackgroundColor: 'rgba(148,159,177,1)',
+      pointBorderColor: '#fff',
+      pointHoverBackgroundColor: '#fff',
+      pointHoverBorderColor: 'rgba(148,159,177,0.8)'
+    },
+    { // red
+      backgroundColor: 'rgba(248,108,107,0.75)',
+      borderColor: 'rgba(196),101,101,1)',
+      pointBackgroundColor: 'rgba(77,83,96,1)',
+      pointBorderColor: '#fff',
+      pointHoverBackgroundColor: '#fff',
+      pointHoverBorderColor: 'rgba(77,83,96,1)'
+    }
+  ];
+  public ratingDistributionLabels: string[] = ['1 star', '2 stars', '3 stars', '4 stars', '5 stars', '6 stars', '7 stars', '8 stars', '9 stars', '10 stars'];
+  public barChartType: string = 'bar';
+  public ratingDistributionLegend: boolean = true;
+
+  public ratingDistributionData: any[] = [
+    {data: [5, 7, 11, 6, 12, 10, 13, 13, 15, 8], label: 'IMDb'},
+    {data: [3, 9, 14, 9, 6, 5, 18, 10, 18, 8], label: 'Trakt.tv'}
+  ];
+
+  // Average Rating
+  public averageRatingOptions: any = {
+    scaleShowVerticalLines: false,
+    responsive: true,
+  };
+  public averageRatingColors: Array<any> = [
+    { // yellow
+      backgroundColor: 'rgba(248, 203, 0, 0.75)',
+      borderColor: 'rgba(211,165,0,1)',
+      pointBackgroundColor: 'rgba(148,159,177,1)',
+      pointBorderColor: '#fff',
+      pointHoverBackgroundColor: '#fff',
+      pointHoverBorderColor: 'rgba(148,159,177,0.8)'
+    },
+    { // red
+      backgroundColor: 'rgba(248,108,107,0.75)',
+      borderColor: 'rgba(196),101,101,1)',
+      pointBackgroundColor: 'rgba(77,83,96,1)',
+      pointBorderColor: '#fff',
+      pointHoverBackgroundColor: '#fff',
+      pointHoverBorderColor: 'rgba(77,83,96,1)'
+    }
+  ];
+  public averageRatingLabels: string[] = ['Episode 1', 'Episode 2', 'Episode 3', 'Episode 4', 'Episode 5', 'Episode 6','Episode 7', 'Episode 8', 'Episode 9', 'Episode 10'];
+  //public barChartType: string = 'bar';
+  public averageRatingLegend: boolean = true;
+
+  public averageRatingData: any[] = [
+    {data: [6.5, 7.3, 7.8, 8.3, 8.0, 9.3, 7.8, 8.3, 8.0, 9.3], label: 'IMDb'},
+    {data: [6.0, 7.9, 7.0, 9.3, 9.3, 9.5, 6.5, 7.3, 7.8, 9.8], label: 'Trakt.tv'}
+  ];
+
   public show = {
     'id': 1,
     'name': 'Game of Thrones',
@@ -48,7 +112,23 @@ export class SeasonComponent implements OnInit {
       }, {
         id: 5,
         name: 'Test'
-      }]
+      },
+        {
+          id: 6,
+          name: 'Test'
+        }, {
+          id: 7,
+          name: 'Test'
+        }, {
+          id: 8,
+          name: 'Test'
+        }, {
+          id: 9,
+          name: 'Test'
+        }, {
+          id: 10,
+          name: 'Test'
+        }]
     }, {
       id: 2,
       episodes: [{
